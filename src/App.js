917aab20d5f8a23b29facad2fd7f3bb773dbc6db
Dewan-toro <dewantoro477@gmail.com>
2024-03-10 11:35:51 +0700

    feat: create counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [number, setNumber] = useState(0);
+
+  const countNumber = (counter) => {
+    if (counter === "+") return setNumber((state) => state + 1);
+    return setNumber((state) => (state - 1 === -1 ? 0 : state - 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ padding: "20px" }}>
+      <h1>Counter</h1>
+      <div style={{ display: "flex", alignItem: "center" }}>
+        <button
+          type="button"
+          onClick={() => countNumber("-")}
+          style={{ marginRight: "10px" }}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <h2 style={{ padding: "0", margin: "0" }}>{number}</h2>
+        <button
+          type="button"
+          onClick={() => countNumber("+")}
+          style={{ marginLeft: "10px" }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
