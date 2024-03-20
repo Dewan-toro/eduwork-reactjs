5e2d79c300e308c531b763429ede1ee3469d84a2
Dewan-toro <dewantoro477@gmail.com>
2024-03-20 21:00:04 +0700

    feat: tugas styling omponents - refactoring

---
+++ src/02_Tugas_Styling_Component/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarComponent from "./navbar";
+import Jumbotron from "./jumbotron";
+import Main from "./main";
+
+export default function StylingComponent() {
+  return (
+    <>
+      <NavbarComponent />
+      <Jumbotron />
+      <Main />
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import Counter from "./01_Basic_Component/Counter";
+// import Counter from "./01_Basic_Component/Counter";
+import StylingComponent from "./02_Tugas_Styling_Component";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  return <Counter nama="Dewantoro" />;
+  return (
+    <>
+      {/* <Counter nama="Dewantoro" /> */}
+      <StylingComponent />
+    </>
+  );
 }
